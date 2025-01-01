ed55a651319a33b8eafed0ca0cf4710c916b8c7e
Rishish <rishish2712@gmail.com>
2025-01-01 22:34:10 +0530

    Update maxscore.py

--- maxscore.py
+++ maxscore.py
@@ -4,14 +4,14 @@ class Solution:
         one_count = s.count('1')
 
         zero_count = 0
-        max_score = 0
+        score = 0
 
         for i in range(n - 1):
             if s[i] == '1':
                 one_count -= 1
             else:
                 zero_count += 1
-            max_score = max(max_score, zero_count + one_count)
+            score = max(score, zero_count + one_count)
 
-        return max_score
+        return score
 
