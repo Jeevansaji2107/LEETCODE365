37cc1bc4a2fe827f10ad22f68b82953989423a77
JEEVAN SAJI <jeevansaji2107@gmail.com>
2025-01-05 21:51:46 +0530

    fourth

---
+++ countpalindromicsubsequence.py
@@ -0,0 +1,10 @@
+#DAY-04
+#UNIQUE LENGTH-3 PALINDROMIC SUBSEQUENCES
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        res = 0
+        for c in set(s):  # Iterate over unique characters
+            i, j = s.find(c), s.rfind(c)  # First and last occurrence
+            if j > i + 1:  # Ensure there's at least one element between
+                res += len(set(s[i+1:j]))  # Count unique middle elements
+        return res
