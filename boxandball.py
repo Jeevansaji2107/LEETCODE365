e0ea0098cb0b4138ba295bd58c85db84ffa218b4
JEEVAN SAJI <jeevansaji2107@gmail.com>
2025-01-06 21:28:58 +0530

    sixth

---
+++ boxandball.py
@@ -0,0 +1,24 @@
+#DAY-06
+#minimum number of operations to move all balls to each box
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        res = [0] * n
+
+        val = 0
+        count = 0
+        for i in range(n):
+            res[i] += val
+            if boxes[i] == '1':
+                count += 1
+            val += count
+
+        val = 0
+        count = 0
+        for i in range(n - 1, -1, -1):
+            res[i] += val
+            if boxes[i] == '1':
+                count += 1
+            val += count
+
+        return res
