643b1e50a1489ccac5e09da59170b6a106ff2ac2
JEEVAN SAJI <jeevansaji2107@gmail.com>
2025-02-10 20:14:42 +0530

    Create cleardigits.py

---
+++ cleardigits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        l=[]
+        for i in s:
+            if(i.isalpha()):
+                l.append(i)
+            else:
+                l.pop()
+        return ''.join(l)class Solution:
+    def clearDigits(self, s: str) -> str:
+        l=[]
+        for i in s:
+            if(i.isalpha()):
+                l.append(i)
+            else:
+                l.pop()
+        return ''.join(l)
